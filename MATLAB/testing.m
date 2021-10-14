b70c0b55ed6070c745706779a7a0031abb500037
twyunting <yc6705a@american.edu>
2021-10-14 14:20:09 -0400

    tmp midterm

---
+++ MATLAB/testing.m
@@ -0,0 +1,41 @@
+N=30;
+K=5;
+l=100;
+rep = 500;
+
+theta = zeros(l,1);
+theta(1:K) = randn(K,1);
+%disp(theta)
+X = randn(N,l)*(1/sqrt(N));
+y = X*theta;
+%disp(size(y))
+
+% Question (a)
+%sols = SolveLasso(X, y);
+%error =norm(sols-theta);
+%disp(error)
+
+P = randperm(N*l)
+%disp(P)
+% Question (d)
+p = [1,9,25,36,64];
+Err = zeros(rep,1);
+for pval = p
+for epan=1:rep
+
+% Construct sparse sensing matrix
+OK = false;
+while ~OK
+    kk = zeros(N*l,1);
+    P = randperm(N*l);
+    numofzeros = round(N*l*(1-1/sqrt(pval)));
+    P(N*l-numofzeros+1:end) = [];
+    kk(P) = sqrt( (sqrt(pval)/N) )*ones(length(P),1).*sign(randn(length(P),1));
+    X = zeros(N,l);
+    X(1:l*N) = kk;
+    % Check if it is full rank
+    OK = rank(X)==N; 
+end
+
+y = X*theta;
+
