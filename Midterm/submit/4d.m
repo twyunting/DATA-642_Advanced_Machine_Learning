51fd5e4c20628c745698ace7c8b405ec7e5a0161
twyunting <yc6705a@american.edu>
2021-10-15 15:21:43 -0400

    done Latex

---
+++ Midterm/submit/4d.m
@@ -0,0 +1,35 @@
+% Question (d)
+p = [1,9,25,36,64];
+Err = zeros(rep,1);
+for pval = p
+for epan=1:rep
+
+% Construct sparse sensing matrix
+OK = false;
+while ~OK
+    kk = zeros(N*l,1);
+    P = randperm(N*l);
+    numofzeros = round(N*l*(1-1/sqrt(pval)));
+    P(N*l-numofzeros+1:end) = [];
+    kk(P) = sqrt( (sqrt(pval)/N) )*ones(length(P),1).*sign(randn(length(P),1));
+    X = zeros(N,l);
+    X(1:l*N) = kk;
+    % Check if it is full rank
+    OK = rank(X)==N; 
+end
+
+y = X*theta;
+
+sols = SolveLasso(X, y,length(theta),'lasso');
+errorX =norm(sols-theta);
+% disp(errorA)
+    
+Err(epan) = errorX<10^(-8) + 0;
+end
+probsparse = sum(Err)/rep; 
+fprintf('Sparse Sensing Mtx, p=%i: %2.2f \n',pval,probsparse)
+
+
+end
+
+disp('')
